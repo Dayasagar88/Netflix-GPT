5bd796f71d1f74e8a70a3334b8bd54fc58928e7c
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2024-07-30 14:57:01 +0530

    filename changed

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -10,7 +10,7 @@ import {
   signInWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-import { auth } from "../Utils/firebase";
+import { auth } from "../Utils/Firebase";
 import { photoURL } from "../Utils/Constants";
 
 const LoginForm = () => {
